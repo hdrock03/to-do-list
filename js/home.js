29d8dc6eb066a6df75d7bb6fd47bbee4a143b748
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-05-17 13:47:37 +0530

    namesort done

--- js/home.js
+++ js/home.js
@@ -56,7 +56,7 @@ if(taskInput.value === ''){
 const taskValue = taskInput.value;  // "buy milk"
 
 
-// format user input
+// format user input: jo bhi input dalenge usko ek format diye hai 
 
 
 const newTask = {
@@ -64,53 +64,61 @@ const newTask = {
     task : taskValue
 }
 
-// get email from session storage
+// get email from session storage qki is page pe session storage call nh kiye h
 
-const session= sessionStorage.getItem('userEmail');
+const session= sessionStorage.getItem('userEmail'); // yahas email aa jayega
+// console.log(session);
+
+//  us email ke sare data ko local storage se localls me save krnge but wo strng ke form me h to json.parse use kie h taki object me change ho jaye
 const localls= JSON.parse(localStorage.getItem(session)); // json.parse // "{}" -->  {}
-
+// console.log(localls);
 
 // console.log(localls, session);
 
-
+// iske phle wla object ko localstorage se nikalne ke baad usme new input tasks bhi save krna h uske liye spread operator use kiye h
 const data = {  // new object
     ...localls, // spreading the whole object
     tasks:[newTask, ...localls.tasks],  // task ko override kiye qki phle se task present the object me aur usme jake new task add kr diye
 }
 
-
+// local storage me fir send karenge data ko
 localStorage.setItem(session,JSON.stringify(data)) // {} -->  "{}"
-    renderList(); // render the new updated data
-}
 
-// target delete button
+    renderList(); // render the new updated data
+
+
+    // delete button -> isko add function ke under rahe hai qki jb bhi koi new task add krnege to sbpe delete event listener add ho jaye
 
 const deleteBtnNodelist = document.querySelectorAll(".fa-remove"); // meko yaha all lagana pada qki list baht sara h iske karan nodelist mila mko to foreach ke
-                                                                   // sath mko lagana pda eventlistener
+// sath mko lagana pda eventlistener
 
 // console.log(deleteBtnNodelist);
 
 deleteBtnNodelist.forEach(item => {
-    item.addEventListener("click", remove);
+item.addEventListener("click", remove);
 })
+}
+
+
 
 
 
 function remove(e) {
     console.log("sex");   
     //  console.log(e.target.id)
+    console.log( e.target.parentElement.parentElement);
     // console.log(e.target.id);
-    e.target.parentElement.parentElement.remove();
+    e.target.parentElement.parentElement.remove();// isse ui se task delete ho jayega but local storage se nh hga delete
 
+    // fr se hmlog ko target krna pdega useremail ko uske lie session storage ko target krna pdega yaha se user email target hua fr
     const sessionlist = sessionStorage.getItem('userEmail');
     // const listArray  = JSON.parse(localStorage.getItem(sessionlist)).tasks;
+
+    // yaha se object ko target kiye
     const listArray  = JSON.parse(localStorage.getItem(sessionlist)) // yeh list array nh object hai 
-    console.log(listArray);
+    // console.log(listArray);
 
-    
-
-
-    // use array.filter on tasks array and save it in a variable
+     // use array.filter on tasks array and save it in a variable
     const filteredArray = listArray.tasks.filter((item)=> {
 
         return item.id != e.target.id; // hm harek element ko array me daal rhe hai jiska id match nh kr rha h
@@ -120,25 +128,71 @@ function remove(e) {
     }) 
 
     listArray.tasks = filteredArray; // yaha jo bhi filtered array hai usko taskilist ke task arrray me save kr rhe
+
     // yha again hmko local storage me data bhejna hga usko lie mko us user ka id khojna hga to uske liye session storage se email target krenge aur waha list array ko daal denge
     localStorage.setItem(sessionStorage.getItem('userEmail'), JSON.stringify(listArray))
 
 
 
+}
 
+reset.addEventListener('click', deleteAllTasks);
 
-    // console.log(filteredArray,"kakak")
-    // function filterItem(idi){
-    //     return idi > 300 ;
-    // }
-    // console.log(filteredArray)
+function deleteAllTasks(e) {
+ if(confirm("are you sure?")){
+    try {
 
-    // put this variable in localstorage
+        taskList.innerHTML= '';
+        const session1 = sessionStorage.getItem('userEmail');
+        const taskObject = JSON.parse(localStorage.getItem(session1));
+        taskObject.tasks= [];
+       
     
+        localStorage.setItem(session1,JSON.stringify(taskObject))
 
-    // call renderList function to render updated task list
+    } catch (error) {
+
+        console.log(error.message)
+        
+    }
+}
 
 }
+
+// sort by name
+
+name.addEventListener('click', sortByName)
+
+function sortByName() {
+
+    const session2 = sessionStorage.getItem('userEmail');
+    const taskObject1 = JSON.parse(localStorage.getItem(session2));
+    let taskArray1 = taskObject1.tasks;
+    console.log(taskArray1);
+     taskArray1.sort((a,b) => {
+        if (a.task < b.task)
+            return -1;
+        if (a.task > b.task)
+            return 1;
+        return 0;
+    });
+    // console.log(sortedArray);
+
+    let htmlString = ``
+
+    taskArray1.forEach(item => {
+       console.log(item.task);
+       htmlString += ` <li>${item.task}</li>`
+    
+       
+    })
+    console.log(htmlString);
+    taskList.innerHTML = htmlString
+    // localStorage.setItem(session2.JSON.stringify(taskObject1))
+
+
+}
+
 
 
 
--- js/register.js
+++ js/register.js
@@ -6,6 +6,7 @@ const confirmPwd= document.querySelector('.confirmpwd');
 const btn= document.querySelector('.btn');
 const form= document.querySelector('.form');
 
+// yaha pe button pe event listener nh lgega jb bhi form ke under koi bhi button lgana hga tb form pe eventlistener lgega
 form.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -14,6 +15,7 @@ form.addEventListener('submit', e => {
         return;
     }
 
+    // yeh object banaye hai taki user jab user login kre to local storage me is format me saved ho
     const userData = {
         isLoggedIn: true,
         firstname : fname.value,
@@ -22,7 +24,13 @@ form.addEventListener('submit', e => {
         password : pwd.value,
         tasks: []
     }
+
+    // localStorage me key value pair me saved hota hai iske liye email.value dale hai key me qki email sbka alg alg hota h 
+    // userdata ko stringyfy lgaye hai qki wo object hai aur local storage me string save hota hai to hair user ke email ke sath uska data 
+    // userData ke format me saved hoga
     localStorage.setItem(email.value, JSON.stringify(userData));
+
+    // session storage me userEmail ke naam se save kiye taki pata chale kon user avi active h
     sessionStorage.setItem('userEmail', email.value)
 
     // window.location.replace("http://127.0.0.1:5503/login.html");
